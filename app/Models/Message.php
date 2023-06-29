36dfb90d2885272ec686d5137720706cc0a7c4b7
RobertoNesta <robertonesta1993@gmail.com>
2023-06-29 17:42:14 +0200

    add apartments():belongstomany function in Message.php file to create a relation between messages and apartments

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Message extends Model
 {
     use HasFactory;
 
     protected $fillable = ['apartment_id', 'email', 'message', 'name', 'lastname'];
+
+    public function apartments(): BelongsToMany
+    {
+        return $this->belongsToMany(Apartment::class);
+    }
 }
