b2b6ca5a135a83b046c89aa595abbad33b7aaf48
fabiodelli <ntqdel@hotmail.com>
2023-06-29 17:47:33 +0200

    add relation  belongs to Apartment

--- app/Models/View.php
+++ app/Models/View.php
@@ -3,11 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+
 
 class View extends Model
 {
     use HasFactory;
 
     protected $fillable = ['date','ip','apartment_id'];
+    /**
+     * The views that belong to the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function views():BelongsTo
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
