741bba3d7a3fbf76c90a5697c087f3c03cfff726
luca-macedone <luca.macedone@outlook.it>
2023-06-30 11:21:05 +0200

    setup ApartmentSeeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Apartment;
 use Illuminate\Database\Seeder;
 
 class ApartmentSeeder extends Seeder
@@ -14,6 +14,20 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $apartments = config('apartments');
+        foreach ($apartments as $apartment) {
+            $new_apartment = new Apartment();
+            $new_apartment->title = $apartment['title'];
+            $new_apartment->slug = Apartment::generateSlug($new_apartment->title);
+            $new_apartment->rooms = $apartment['rooms'];
+            $new_apartment->beds = $apartment['beds'];
+            $new_apartment->square_meters = $apartment['square_meters'];
+            $new_apartment->bathrooms = $apartment['bathrooms'];
+            $new_apartment->image = $apartment['image'];
+            $new_apartment->latitude = $apartment['latitude'];
+            $new_apartment->longitude = $apartment['longitude'];
+            $new_apartment->full_address = $apartment['full_address'];
+            $new_apartment->save();
+        }
     }
 }
