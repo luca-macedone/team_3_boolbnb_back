4f6c524ace6451f5e6cf48bd65f73d50141d78ad
luca-macedone <luca.macedone@outlook.it>
2023-07-02 19:54:23 +0200

    add apartment crud views

---
+++ resources/views/user/apartments/edit.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                <strong>Error</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ route('user.apartments.update', $apartment) }}" method="post">
+            @csrf
+            @method('PUT')
+            {{-- title --}}
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId"
+                    placeholder="" value="{{ $apartment->title }}">
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            {{-- rooms --}}
+            <div class="mb-3">
+                <label for="rooms" class="form-label">Rooms</label>
+                <input type="number" min="1" step="1" class="form-control" name="rooms" id="rooms"
+                    aria-describedby="helpId" placeholder="" value="{{ $apartment->rooms }}">
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            {{-- beds --}}
+            <div class="mb-3">
+                <label for="beds" class="form-label">Beds</label>
+                <input type="number" min="1" step="1" class="form-control" name="beds" id="beds"
+                    aria-describedby="helpId" placeholder="" value="{{ $apartment->beds }}">
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            {{-- square meters --}}
+            <div class="mb-3">
+                <label for="square_meters" class="form-label">Square meters</label>
+                <input type="number" min="8" step="10" class="form-control" name="square_meters"
+                    id="square_meters" aria-describedby="helpId" placeholder="" value="{{ $apartment->square_meters }}">
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            {{-- bathrooms --}}
+            <div class="mb-3">
+                <label for="bathrooms" class="form-label">Bathrooms</label>
+                <input type="number" min="1" step="1" class="form-control" name="bathrooms" id="bathrooms"
+                    aria-describedby="helpId" placeholder=""
+                    value="{{ $apartment->bathrooms }}>
+                <small id="helpId" class="form-text text-muted">Help
+                text</small>
+            </div>
+            {{-- image --}}
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="file" class="form-control" name="image" id="image" aria-describedby="helpId"
+                    placeholder="">
+                <small id="helpId" class="form-text text-muted">Help text</small>
+                {{-- ! da mettere la preview dell'immagine già presente, se c'è --}}
+            </div>
+            {{-- full address --}}
+            <div class="mb-3">
+                <label for="full_address" class="form-label">Full address</label>
+                <input type="text" class="form-control" name="full_address" id="full_address" aria-describedby="helpId"
+                    placeholder="" value="{{ $apartment->full_address }}" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            {{-- visibility --}}
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="true" id="visibility"
+                    @if ($apartment->visibility == '1') checked @endif>
+                <label class="form-check-label" for="visibility">
+                    Is visible
+                </label>
+            </div>
+            {{-- services --}}
+            <label for="services" class="form-label">Services</label>
+            <div class="d-flex justify-content-start align-items-center flex-wrap gap-3 mb-5">
+                @forelse ($services as $index => $service)
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="{{ $service->id }}"
+                            id="{{ 'service' . $index }}" name="services[]"
+                            {{ $apartment->services->contains($service) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="{{ 'service' . $index }}">
+                            {{ $service->name }}
+                        </label>
+                    </div>
+                @empty
+                @endforelse
+
+            </div>
+
+            <a type="button" href="{{ route('user.apartments.index') }}" class="btn btn-primary">Back</a>
+            <button type="reset" class="btn btn-primary">Reset</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+    </div>
+@endsection
---
+++ resources/views/user/apartments/index.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                @if (session('message'))
+                    <div class="col-12 pt-3">
+                        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            <strong>Success! </strong>
+                            {{ session('message') }}
+                        </div>
+                    </div>
+                @endif
+                <div class="col d-flex justify-content-end p-3">
+                    <a type="button" class="btn btn-outline-dark" href="{{ route('user.apartments.create') }}">Add</a>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Rooms</th>
+                        <th scope="col">Full Address</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($apartments as $apartment)
+                        <tr class="">
+                            <td scope="row">{{ $apartment->title }}</td>
+                            <td>{{ $apartment->rooms }}</td>
+                            <td>{{ $apartment->full_address }}</td>
+                            <td>
+                                <div class="d-flex align-items-center gap-2">
+                                    <a type="button" class="btn btn-outline-dark"
+                                        href="{{ route('user.apartments.show', $apartment->slug) }}">watch</a>
+                                    <a type="button" class="btn btn-outline-dark"
+                                        href="{{ route('user.apartments.edit', $apartment->slug) }}">edit</a>
+                                    <button type="button" class="btn btn-outline-danger d-flex align-items-center gap-1"
+                                        data-bs-toggle="modal" data-bs-target="{{ '#modal' . $apartment->id }}"
+                                        title="{{ __('Delete') }}">
+                                        delete
+                                    </button>
+                                    <div class="modal fade" id="{{ 'modal' . $apartment->id }}" tabindex="-1"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                        aria-labelledby="{{ 'modalTitle' . $apartment->id }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                            role="document">
+                                            <div class="modal-content rounded-0">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title text-danger"
+                                                        id="{{ 'modalTitle' . $apartment->id }}">{{ __('Danger Zone') }}
+                                                    </h5>
+                                                    <button type="button" class="btn-close rounded-0"
+                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div>{{ __('This operation is irreversible.') }}</div>
+                                                    <div class="fw-semibold">{{ __('Are you sure?') }}</div>
+                                                </div>
+                                                <div class="modal-footer d-flex justify-content-between">
+                                                    <button type="button" class="rounded-0 btn btn-outline-dark"
+                                                        data-bs-dismiss="modal">{{ __('Close') }}</button>
+                                                    <form action="{{ route('user.apartments.destroy', $apartment) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="rounded-0 btn btn-outline-danger">
+                                                            <i class="fa-solid fa-trash me-1"></i>
+                                                            {{ __('Delete permanently') }}
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr class="">
+                            <td scope="row">Nothing here yet!</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
