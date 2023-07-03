7a948a0ddc3fb503c9c20016012827570c1cb214
Dan97O <dan97-oddo@live.it>
2023-07-03 13:55:03 +0200

    edit page 'edit' with error validation and view image

--- resources/views/user/apartments/edit.blade.php
+++ resources/views/user/apartments/edit.blade.php
@@ -2,75 +2,71 @@
 
 @section('content')
     <div class="container py-5">
-        @if ($errors->any())
-            <div class="alert alert-danger" role="alert">
-                <strong>Error</strong>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form action="{{ route('user.apartments.update', $apartment) }}" method="post">
+        @include('partials.validation_errors')
+        <form action="{{ route('user.apartments.update', $apartment) }}" method="post" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             {{-- title --}}
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId"
-                    placeholder="" value="{{ $apartment->title }}">
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                <input type="text" class="form-control  @error('title') is-invalid @enderror" name="title" id="title"
+                    aria-describedby="helpId" placeholder="" value="{{ $apartment->title }}">
+                <small id="helpId" class="form-text text-muted">Name required, maximum 255 characters</small>
             </div>
             {{-- rooms --}}
             <div class="mb-3">
                 <label for="rooms" class="form-label">Rooms</label>
-                <input type="number" min="1" step="1" class="form-control" name="rooms" id="rooms"
+                <input type="number" min="1" step="1"
+                    class="form-control @error('rooms') is-invalid @enderror" name="rooms" id="rooms"
                     aria-describedby="helpId" placeholder="" value="{{ $apartment->rooms }}">
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
             </div>
             {{-- beds --}}
             <div class="mb-3">
                 <label for="beds" class="form-label">Beds</label>
-                <input type="number" min="1" step="1" class="form-control" name="beds" id="beds"
-                    aria-describedby="helpId" placeholder="" value="{{ $apartment->beds }}">
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                <input type="number" min="1" step="1" class="form-control @error('beds') is-invalid @enderror"
+                    name="beds" id="beds" aria-describedby="helpId" placeholder="" value="{{ $apartment->beds }}">
+                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
             </div>
             {{-- square meters --}}
             <div class="mb-3">
                 <label for="square_meters" class="form-label">Square meters</label>
-                <input type="number" min="8" step="10" class="form-control" name="square_meters"
+                <input type="number" min="8" step="10"
+                    class="form-control @error('square_meters') is-invalid @enderror" name="square_meters"
                     id="square_meters" aria-describedby="helpId" placeholder="" value="{{ $apartment->square_meters }}">
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                <small id="helpId" class="form-text text-muted">Insert a numerical value, minimum 8 square meters</small>
             </div>
             {{-- bathrooms --}}
             <div class="mb-3">
                 <label for="bathrooms" class="form-label">Bathrooms</label>
-                <input type="number" min="1" step="1" class="form-control" name="bathrooms" id="bathrooms"
-                    aria-describedby="helpId" placeholder=""
-                    value="{{ $apartment->bathrooms }}>
-                <small id="helpId" class="form-text text-muted">Help
-                text</small>
+                <input type="number" min="1" step="1"
+                    class="form-control @error('bathrooms') is-invalid @enderror" name="bathrooms" id="bathrooms"
+                    aria-describedby="helpId" placeholder="" value="{{ $apartment->bathrooms }}">
+                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
             </div>
             {{-- image --}}
-            <div class="mb-3">
-                <label for="image" class="form-label">Image</label>
-                <input type="file" class="form-control" name="image" id="image" aria-describedby="helpId"
-                    placeholder="">
-                <small id="helpId" class="form-text text-muted">Help text</small>
-                {{-- ! da mettere la preview dell'immagine già presente, se c'è --}}
+            <div class="d-flex gap-3 mb-3">
+                <img width="100" src="{{ asset('storage/' . $apartment->image) }}" alt="">
+                <div class="mb-3">
+                    <label for="image" class="form-label">Image</label>
+                    <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
+                        id="image" aria-describedby="imageHelper">
+                    <small id="imageHelper" class="form-text text-muted">
+                        Insert the image here
+                    </small>
+                </div>
             </div>
             {{-- full address --}}
             <div class="mb-3">
                 <label for="full_address" class="form-label">Full address</label>
-                <input type="text" class="form-control" name="full_address" id="full_address" aria-describedby="helpId"
-                    placeholder="" value="{{ $apartment->full_address }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                <input type="text" class="form-control @error('full_address') is-invalid @enderror" name="full_address"
+                    id="full_address" aria-describedby="helpId" placeholder="" value="{{ $apartment->full_address }}" />
+                <small id="helpId" class="form-text text-muted">Full address required, maximum 255 characters</small>
             </div>
             {{-- visibility --}}
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" id="visibility"
-                    @if ($apartment->visibility == '1') checked @endif>
+            <div class="form-check pb-3">
+                <input class="form-check-input @error('visibility') is-invalid @enderror" type="checkbox" value="true"
+                    id="visibility" @if ($apartment->visibility == '1') checked @endif>
                 <label class="form-check-label" for="visibility">
                     Is visible
                 </label>
@@ -89,13 +85,10 @@
                     </div>
                 @empty
                 @endforelse
-
             </div>
-
             <a type="button" href="{{ route('user.apartments.index') }}" class="btn btn-primary">Back</a>
             <button type="reset" class="btn btn-primary">Reset</button>
             <button type="submit" class="btn btn-primary">Submit</button>
-
         </form>
     </div>
 @endsection
