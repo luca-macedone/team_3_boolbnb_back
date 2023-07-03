6df3f64b17c33cf0eace073839ae9acb4918dc23
lucaruboni <lucaruboni@gmail.com>
2023-07-03 18:27:02 +0200

    [add] layout initial style

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
+
+        
+        <!-- right -->
+
+        <div class="">
+            <div class="container">
+
+                
+
+                <div class="user_name">{{ Auth::user()->name }}</div>
+
+                <div class="">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
@@ -23,4 +26,6 @@
         </div>
     </div>
 </div>
+
+
 @endsection
