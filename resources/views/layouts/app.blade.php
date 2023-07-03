f4be98d55ec7f9533511857b2adc472bc29593f3
luca-macedone <luca.macedone@outlook.it>
2023-07-03 15:27:35 +0200

    fix img display error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/scss/img_wrapper.scss'])
 </head>
 
 <body>
