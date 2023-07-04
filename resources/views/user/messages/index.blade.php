cebf1a2c6b3e211e18c284757d1fe632593d1e32
Dan97O <dan97-oddo@live.it>
2023-07-04 16:01:38 +0200

    add page index message

--- resources/views/user/messages/index.blade.php
+++ resources/views/user/messages/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Message</p>
+    <div class="container">
+        <h1>My Messages</h1>
+        @include('partials.session_message')
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Message</th>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Lastname</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($messages as $message)
+                    <tr>
+                        <td>
+                            <textarea readonly rows="4" style="resize: none; width: 100%">{{ $message->message }}</textarea>
+                        </td>
+                        <td>{{ $message->email }}</td>
+                        <td>{{ $message->name }}</td>
+                        <td>{{ $message->lastname }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5">No messages found.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
