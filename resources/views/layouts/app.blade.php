32fd510d3fe0582f5f2ed54470a145cf35d2a0ed
luca-macedone <luca.macedone@outlook.it>
2023-07-04 11:05:16 +0200

    add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,52 +27,78 @@
 
 <body>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-xl navbar-light shadow">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <div class="logo_laravel">
+        <div class="offcanvas_navbar p-2 shadow d-flex justify-content-start align-items-center gap-3">
+            <a class="d-xl-none btn offcanvas_button" data-bs-toggle="offcanvas" href="#offcanvas" role="button"
+                aria-controls="offcanvasExample">
+                <i class="fa-solid fa-bars fa-2x"></i>
+            </a>
+            <div class="logo_wrapper"></div>
+        </div>
 
-                    </div>
-                    
-                </a>
+        <div class="offcanvas offcanvas-start d-xl-none" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+            <div class="offcanvas-header d-flex justify-content-end">
+                {{-- <h5 class="offcanvas-title" id="offcanvasLabel">Boolbnb</h5> --}}
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="d-flex flex-column align-items-start justify-content-start gap-3 ps-0">
+                    @auth
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav d-flex d-xl-none align-items-center gap-3">
-                
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <li>
+                            <i class="fa-solid fa-home"></i>
+                            <a class="" href="{{ url('/') }}">
+                                {{ __('Home') }}
+                            </a>
                         </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            @if (Auth::user()->name)
-                                {{ Auth::user()->name }}
-                            @else
-                                {{ Auth::user()->email }}
-                            @endif
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
-                            
+                        <li>
+                            <i class="fa-solid fa-user"></i>
+                            <a class="" href="{{ url('profile') }}">
+                                {{ __('Profile') }}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-regular fa-building"></i>
+                            <a class="" href="{{ url('/user/apartments') }}">
+                                {{ __('Apartments') }}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-envelope-open-text"></i>
+                            <a class="" href="{{ url('/user/apartments') }}">
+                                {{ __('Messages') }}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-tv"></i>
+                            <a class="" href="{{ url('/user/services') }}">
+                                {{ __('Services') }}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-star"></i>
+                            <a class="" href="{{ url('/user/services') }}">
+                                {{ __('Sponsorships') }}
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-chart-pie"></i>
+                            <a class="" href="{{ url('/user/services') }}">
+                                {{ __('Analytics') }}
+                            </a>
+                        </li>
+                    @endauth
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li>
+                            <i class="fa-solid fa-right-from-bracket"></i>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -82,69 +108,8 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
-    </nav> --}}
-        <div class="offcanvas_navbar p-2 d-xl-none shadow">
-            <a class="btn btn-outline-light offcanvas_button" data-bs-toggle="offcanvas" href="#offcanvas"
-                role="button" aria-controls="offcanvasExample">
-                <i class="fa-solid fa-bars fa-2x"></i>
-            </a>
-        </div>
-
-        <div class="offcanvas offcanvas-start d-xl-none" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasLabel">Offcanvas</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <ul class="d-flex flex-column align-items-start justify-content-start gap-3 ps-0">
-                    <li>
-                        <i class="fa-solid fa-home"></i>
-                        <a class="" href="{{ url('/') }}">
-                            {{ __('Home') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-user"></i>
-                        <a class="" href="{{ url('profile') }}">
-                            {{ __('Profile') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-regular fa-building"></i>
-                        <a class="" href="{{ url('/user/apartments') }}">
-                            {{ __('Apartments') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-envelope-open-text"></i>
-                        <a class="" href="{{ url('/user/apartments') }}">
-                            {{ __('Messages') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-tv"></i>
-                        <a class="" href="{{ url('/user/services') }}">
-                            {{ __('Services') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-star"></i>
-                        <a class="" href="{{ url('/user/services') }}">
-                            {{ __('Sponsorships') }}
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fa-solid fa-chart-pie"></i>
-                        <a class="" href="{{ url('/user/services') }}">
-                            {{ __('Analytics') }}
-                        </a>
-                    </li>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
@@ -157,48 +122,79 @@
                 <div class="col-12 col-lg-3 d-none d-xl-block">
                     <aside class="sidebar_menu px-5 py-4 shadow">
                         <ul class="d-flex flex-column align-items-start justify-content-start gap-3 ps-0">
-                            <li>
-                                <i class="fa-solid fa-home"></i>
-                                <a class="" href="{{ url('/') }}">
-                                    {{ __('Home') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-user"></i>
-                                <a class="" href="{{ url('profile') }}">
-                                    {{ __('Profile') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-regular fa-building"></i>
-                                <a class="" href="{{ url('/user/apartments') }}">
-                                    {{ __('Apartments') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-envelope-open-text"></i>
-                                <a class="" href="{{ url('/user/apartments') }}">
-                                    {{ __('Messages') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-tv"></i>
-                                <a class="" href="{{ url('/user/services') }}">
-                                    {{ __('Services') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-star"></i>
-                                <a class="" href="{{ url('/user/services') }}">
-                                    {{ __('Sponsorships') }}
-                                </a>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-chart-pie"></i>
-                                <a class="" href="{{ url('/user/services') }}">
-                                    {{ __('Analytics') }}
-                                </a>
-                            </li>
+                            {{-- <li>
+                                <div class="logo_wrapper">
+                                    
+                                </div>
+                            </li> --}}
+                            @auth
+                                <li>
+                                    <i class="fa-solid fa-home"></i>
+                                    <a class="" href="{{ url('/') }}">
+                                        {{ __('Home') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-user"></i>
+                                    <a class="" href="{{ url('profile') }}">
+                                        {{ __('Profile') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-regular fa-building"></i>
+                                    <a class="" href="{{ url('/user/apartments') }}">
+                                        {{ __('Apartments') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-envelope-open-text"></i>
+                                    <a class="" href="{{ url('/user/apartments') }}">
+                                        {{ __('Messages') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-tv"></i>
+                                    <a class="" href="{{ url('/user/services') }}">
+                                        {{ __('Services') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-star"></i>
+                                    <a class="" href="{{ url('/user/services') }}">
+                                        {{ __('Sponsorships') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-chart-pie"></i>
+                                    <a class="" href="{{ url('/user/services') }}">
+                                        {{ __('Analytics') }}
+                                    </a>
+                                </li>
+                            @endauth
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li>
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                            @endguest
                         </ul>
                     </aside>
                 </div>
