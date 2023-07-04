2e0c03d87bf7daa106e91c9ea11a2147966cf6a3
luca-macedone <luca.macedone@outlook.it>
2023-07-04 18:28:08 +0200

    some graphic fix

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -1,119 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
 
-                 @if (session('message'))
-                    <div class="col-12 pt-3">
-                        <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            <strong>Success! </strong>
-                            {{ session('message') }}
-                        </div>
-                    </div>
-                @endif
-                <div class="col d-flex justify-content-end p-3">
-                    <a type="button" class="btn btn-dark" href="{{ route('user.apartments.create') }}">Add</a>
+        @if (session('message'))
+            <div class="col-12 pt-3">
+                <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <strong>Success! </strong>
+                    {{ session('message') }}
                 </div>
-            
-        
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gap-5">
-          
-        
-        
-                   
-                    
+            </div>
+        @endif
+        <div class="col d-flex justify-content-end p-3">
+            <a type="button" class="btn btn-dark" href="{{ route('user.apartments.create') }}">Add</a>
+        </div>
 
-                        @forelse ($apartments as $apartment)
 
-                        
-                        <div class="card mb-3 card_padding card_shadow bg_bnb_white card_hover" >
-                            <div class="row ">
-                              <div class="col-md-4">
-                               <div class="img-wrapper rounded-2 card_shadow">
-                                    <img src="{{asset('storage/' . $apartment->image)}}" class="img-fluid" alt="{{$apartment->title}}">
-                               </div>
-                              </div>
-                              <div class="col-md-8">
-                                <div class="card-body">
-                                  <h5 class="card-title">{{$apartment->title}}</h5>
-                                  <p class="card-text">{{$apartment->full_address}}</p>
-                                  <p class="card-text"><small class="text-muted">You have 3 message!</small></p>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-3">
+            @forelse ($apartments as $apartment)
+                <div class="col">
+                    <div class="card h-100 mb-3 p-3 card_shadow bg_bnb_white card_hover">
+                        <div class="row ">
+                            <div class="col-md-4 overflow-hidden">
+                                <div class="img-wrapper overflow-hidden h-100">
+                                    <img src="{{ asset('/storage/uploads/' . $apartment->image) }}" class="object-fit-cover"
+                                        alt="{{ $apartment->title }}" />
                                 </div>
-                        </div>
-
-
-                    <!-- action -->
-                       
-                                <div class="d-flex align-items-center gap-2 ms-1">
-                                    <a class="action_btn action_show p-2"
-                                        href="{{ route('user.apartments.show', $apartment->slug) }}">
-                                            <i class="fa-solid fa-eye"></i>
-                                        </a>
-                                    <a  class="action_btn action_edit p-2"
-                                        href="{{ route('user.apartments.edit', $apartment->slug) }}">
-                                        <i class="fa-solid fa-pen"></i>
-                                    </a>
-                                    <button  class="action_btn action_delete p-2"
-                                        data-bs-toggle="modal" data-bs-target="{{ '#modal' . $apartment->id }}"
-                                        title="{{ __('Delete') }}">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
-
-            <!-- delete modal -->
-
-                                    <div class="modal fade" id="{{ 'modal' . $apartment->id }}" tabindex="-1"
-                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                        aria-labelledby="{{ 'modalTitle' . $apartment->id }}" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                            role="document">
-                                            <div class="modal-content rounded-0">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title text-danger"
-                                                        id="{{ 'modalTitle' . $apartment->id }}">{{ __('Danger Zone') }}
-                                                    </h5>
-                                                    <button type="button" class="btn-close rounded-0"
-                                                        data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div>{{ __('This operation is irreversible.') }}</div>
-                                                    <div class="fw-semibold">{{ __('Are you sure?') }}</div>
-                                                </div>
-                                                <div class="modal-footer d-flex justify-content-between">
-                                                    <button type="button" class="rounded-0 btn btn-outline-dark"
-                                                        data-bs-dismiss="modal">{{ __('Close') }}</button>
-                                                    <form action="{{ route('user.apartments.destroy', $apartment) }}"
-                                                        method="post">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="rounded-0 btn btn-outline-danger">
-                                                            <i class="fa-solid fa-trash me-1"></i>
-                                                            {{ __('Delete permanently') }}
-                                                        </button>
-                                                    </form>
-                                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $apartment->title }}</h5>
+                                    <p class="card-text">{{ $apartment->full_address }}</p>
+                                    <p class="card-text"><small class="text-muted">You have 3 message!</small></p>
+                                </div>
+                            </div>
+                            <!-- action -->
+                            <div class="d-flex align-items-center gap-2 py-2">
+                                <a class="action_btn action_show p-2"
+                                    href="{{ route('user.apartments.show', $apartment->slug) }}">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <a class="action_btn action_edit p-2"
+                                    href="{{ route('user.apartments.edit', $apartment->slug) }}">
+                                    <i class="fa-solid fa-pen"></i>
+                                </a>
+                                <button class="action_btn action_delete p-2" data-bs-toggle="modal"
+                                    data-bs-target="{{ '#modal' . $apartment->id }}" title="{{ __('Delete') }}">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                                <!-- delete modal -->
+                                <div class="modal fade" id="{{ 'modal' . $apartment->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="{{ 'modalTitle' . $apartment->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content rounded-0">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title text-danger"
+                                                    id="{{ 'modalTitle' . $apartment->id }}">
+                                                    {{ __('Danger Zone') }}
+                                                </h5>
+                                                <button type="button" class="btn-close rounded-0" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div>{{ __('This operation is irreversible.') }}</div>
+                                                <div class="fw-semibold">{{ __('Are you sure?') }}</div>
+                                            </div>
+                                            <div class="modal-footer d-flex justify-content-between">
+                                                <button type="button" class="rounded-0 btn btn-outline-dark"
+                                                    data-bs-dismiss="modal">{{ __('Close') }}</button>
+                                                <form action="{{ route('user.apartments.destroy', $apartment) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="rounded-0 btn btn-outline-danger">
+                                                        <i class="fa-solid fa-trash me-1"></i>
+                                                        {{ __('Delete permanently') }}
+                                                    </button>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
-
-        <!-- / delete modal -->
-
-
                                 </div>
-
-                                </div>
-                            
-                          </div>
-                         
-                    @empty
-                        
-                    @endforelse
-             
+                                <!-- / delete modal -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+            @endforelse
         </div>
-
-        </div>
-        <div class="px-3 py-2">
-            {{ $apartments->links('pagination::bootstrap-5') }}
-        </div>
-        
+    </div>
+    <div class="px-3 py-2">
+        {{ $apartments->links('pagination::bootstrap-5') }}
+    </div>
 @endsection
--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -1,41 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row py-5">
+            <div class="col ">
 
-<div class="container">
-    <div class="row py-5">
-        <div class="col ">
-        
-            <img src="{{asset('storage/' . $apartment->image)}}" class="img-fluid" alt="{{$apartment->title}}">
-           
-        </div>
+                <img src="{{ asset('/storage/uploads/' . $apartment->image) }}" class="img-fluid"
+                    alt="{{ $apartment->title }}">
 
-        <div class="col">
-            <h1>{{$apartment->title}}</h1>
-            <div> <strong>rooms: </strong>{{$apartment->rooms}}</div>
-            <div> <strong>beds: </strong>{{$apartment->beds}}</div>
-            <div> <strong>square meters: </strong>{{$apartment->square_meters}}</div>
-            <div> <strong>bathrooms: </strong>{{$apartment->bathrooms}}</div>
-            <div> <strong>full address: </strong>{{$apartment->full_address}}</div>
-
-            
-          
-            <h6><strong>Service:</strong></h6>
-                  <ul>
-                
-                    @forelse ($apartment->services as $service)
-                    
-                    <li>  {{ $service->name }}</li>
-                    @empty
-                      <div><strong>Service:</strong> n/a</div>
-                    @endforelse
-                  <ul>
-             
-          
-          
             </div>
-                       
+
+            <div class="col">
+                <h1>{{ $apartment->title }}</h1>
+                <div> <strong>rooms: </strong>{{ $apartment->rooms }}</div>
+                <div> <strong>beds: </strong>{{ $apartment->beds }}</div>
+                <div> <strong>square meters: </strong>{{ $apartment->square_meters }}</div>
+                <div> <strong>bathrooms: </strong>{{ $apartment->bathrooms }}</div>
+                <div> <strong>full address: </strong>{{ $apartment->full_address }}</div>
+
+
+
+                <h6><strong>Service:</strong></h6>
+                <ul>
+
+                    @forelse ($apartment->services as $service)
+                        <li> {{ $service->name }}</li>
+                    @empty
+                        <div><strong>Service:</strong> n/a</div>
+                    @endforelse
+                    <ul>
+
+
+
+            </div>
+
         </div>
     </div>
-
 @endsection
