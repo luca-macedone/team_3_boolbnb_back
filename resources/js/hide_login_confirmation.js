feeb638360520b0f59bc6ce655674458e28db27a
lucaruboni <lucaruboni@gmail.com>
2023-07-05 17:29:11 +0200

    [add] async function to hide log in message

---
+++ resources/js/hide_login_confirmation.js
@@ -0,0 +1,7 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      const logBox = document.getElementById("log_box");
+      logBox.classList.add("d-none");
+    }, 3000);
+  });
