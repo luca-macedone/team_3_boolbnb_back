845bc75424d8522fb1ea5bd96ebac0e30bdf01e1
luca-macedone <luca.macedone@outlook.it>
2023-07-05 15:17:29 +0200

    add frontend validation to edit

--- resources/views/user/apartments/edit.blade.php
+++ resources/views/user/apartments/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('javascript')
+    @vite(['/resources/js/validations/apartment_validation.js'])
+@endsection
+
 @section('content')
     <div class="container py-5">
         @include('partials.validation_errors')
@@ -11,7 +15,15 @@
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control  @error('title') is-invalid @enderror" name="title" id="title"
                     aria-describedby="helpId" placeholder="" value="{{ $apartment->title }}">
-                <small id="helpId" class="form-text text-muted">Name required, maximum 255 characters</small>
+                <div>
+                    <small id="helpId" class="form-text text-muted">Name required, maximum 255 characters</small>
+                </div>
+                <div id="title_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">
+                        Please insert a valid text (min lenght 1 char, max lenght 255 chars)
+                    </span>
+                </div>
             </div>
             {{-- rooms --}}
             <div class="mb-3">
@@ -19,14 +31,30 @@
                 <input type="number" min="1" step="1"
                     class="form-control @error('rooms') is-invalid @enderror" name="rooms" id="rooms"
                     aria-describedby="helpId" placeholder="" value="{{ $apartment->rooms }}">
-                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                <div>
+                    <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                </div>
+                <div id="rooms_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">
+                        Please insert a valid number (minimum number is 1)
+                    </span>
+                </div>
             </div>
             {{-- beds --}}
             <div class="mb-3">
                 <label for="beds" class="form-label">Beds</label>
                 <input type="number" min="1" step="1" class="form-control @error('beds') is-invalid @enderror"
                     name="beds" id="beds" aria-describedby="helpId" placeholder="" value="{{ $apartment->beds }}">
-                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                <div>
+                    <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                </div>
+                <div id="beds_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">
+                        Please insert a valid number (minimum number is 1)
+                    </span>
+                </div>
             </div>
             {{-- square meters --}}
             <div class="mb-3">
@@ -34,7 +62,16 @@
                 <input type="number" min="8" step="10"
                     class="form-control @error('square_meters') is-invalid @enderror" name="square_meters"
                     id="square_meters" aria-describedby="helpId" placeholder="" value="{{ $apartment->square_meters }}">
-                <small id="helpId" class="form-text text-muted">Insert a numerical value, minimum 8 square meters</small>
+                <div>
+                    <small id="helpId" class="form-text text-muted">Insert a numerical value, minimum 8 square
+                        meters</small>
+                </div>
+                <div id="square_meters_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">
+                        Please insert a valid number (minimum number is 8)
+                    </span>
+                </div>
             </div>
             {{-- bathrooms --}}
             <div class="mb-3">
@@ -42,7 +79,15 @@
                 <input type="number" min="1" step="1"
                     class="form-control @error('bathrooms') is-invalid @enderror" name="bathrooms" id="bathrooms"
                     aria-describedby="helpId" placeholder="" value="{{ $apartment->bathrooms }}">
-                <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                <div>
+                    <small id="helpId" class="form-text text-muted">Insert a numerical value</small>
+                </div>
+                <div id="bathrooms_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">
+                        Please insert a valid number (minimum number is 1)
+                    </span>
+                </div>
             </div>
             {{-- image --}}
             <div class="d-flex gap-3 mb-3">
@@ -59,17 +104,25 @@
                     <label for="image" class="form-label">Image</label>
                     <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
                         id="image" aria-describedby="imageHelper">
-                    <small id="imageHelper" class="form-text text-muted">
-                        Insert the image here
-                    </small>
+                    <div>
+                        <small id="helpId" class="form-text text-muted">Insert the image here</small>
+                    </div>
                 </div>
             </div>
             {{-- full address --}}
             <div class="mb-3">
                 <label for="full_address" class="form-label">Full address</label>
-                <input type="text" class="form-control @error('full_address') is-invalid @enderror" name="full_address"
-                    id="full_address" aria-describedby="helpId" placeholder="" value="{{ $apartment->full_address }}" />
-                <small id="helpId" class="form-text text-muted">Full address required, maximum 255 characters</small>
+                <input type="text" class="form-control @error('full_address') is-invalid @enderror"
+                    name="full_address" id="full_address" aria-describedby="helpId" placeholder=""
+                    value="{{ $apartment->full_address }}" />
+                <div>
+                    <small id="helpId" class="form-text text-muted">Full address required, maximum 255
+                        characters</small>
+                </div>
+                <div id="full_address_error" class="alert alert-danger border-0 my-1 d-none">
+                    <strong class="fw-semibold">Error! </strong>
+                    <span class="fw-lighter">Please insert a valid text (min lenght 1 char, max lenght 255 chars)</span>
+                </div>
             </div>
             {{-- visibility --}}
             <div class="form-check pb-3">
@@ -94,9 +147,15 @@
                 @empty
                 @endforelse
             </div>
-            <a type="button" href="{{ route('user.apartments.index') }}" class="btn btn-primary">Back</a>
-            <button type="reset" class="btn btn-primary">Reset</button>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div id="services_error" class="alert alert-danger border-0 my-1 d-none">
+                <strong class="fw-semibold">Error! </strong>
+                <span class="fw-lighter">Please select at least 1 service</span>
+            </div>
+            <div class="mt-3">
+                <a type="button" href="{{ route('user.apartments.index') }}" class="btn btn-primary">Back</a>
+                <button type="reset" class="btn btn-primary">Reset</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
         </form>
     </div>
 @endsection
