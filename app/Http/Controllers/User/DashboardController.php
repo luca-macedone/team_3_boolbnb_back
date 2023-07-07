b340d9f6a14d574122b798a3bae1d23fda024db3
luca-macedone <luca.macedone@outlook.it>
2023-07-07 11:38:31 +0200

    add apartment in dashboard

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard');
+        // $user_id = Auth::user();
+        $apartment = Auth::user()?->apartments()->first();
+
+        // dd($apartment);
+
+        return view('user.dashboard', compact('apartment'));
     }
 
     public function front_office()
