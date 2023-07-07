2d6c4a484e39f5f8e42f285681a1d68f8746ddff
luca-macedone <luca.macedone@outlook.it>
2023-07-07 11:51:56 +0200

    add new api controller and endpoint

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function user_data()
+    {
+
+        if (Auth::user()) {
+            if (Auth::user()->name || Auth::user()->lastname) {
+                $user_name = Auth::user()->name;
+                $user_lastname = Auth::user()->lastname;
+
+                return response()->json([
+                    'success' => true,
+                    'user' => [
+                        'name' => $user_name,
+                        'lastname' => $user_lastname,
+                    ],
+                ]);
+            } else {
+                $user_email = Auth::user()->email;
+
+                return response()->json([
+                    'success' => true,
+                    'user' => [
+                        'email' => $user_email,
+                    ],
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'user' => null,
+            ]);
+        }
+    }
+}
