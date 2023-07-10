e2d448810785481ac1fa0f5ab320e4ea3fb4f309
luca-macedone <luca.macedone@outlook.it>
2023-07-10 16:55:32 +0200

    add generic search

--- app/Http/Controllers/API/ApartmentController.php
+++ app/Http/Controllers/API/ApartmentController.php
@@ -18,6 +18,7 @@ class ApartmentController extends Controller
         // $apartments = Apartment::with(['services', 'views', 'sponsorships'])->orderByDesc('id')->paginate(12);
         // $apartments = Apartment::with(['services'])->orderByDesc('id')->paginate(12);
         // dd($apartments);
+        $generic_search = boolval($request->query('generic_search'));
         $left_lat = floatval($request->query('left_lat'));
         $right_lat = floatval($request->query('right_lat'));
         $left_lon = floatval($request->query('left_lon'));
@@ -27,27 +28,30 @@ class ApartmentController extends Controller
         $services = $request->query('services');
 
         $query = Apartment::query()->with('services');
+        if ($generic_search) {
+            $query->where('id', '>', 0)->orderByDesc('id');
+        } else {
+            if (! empty($left_lat) && ! empty($left_lon) && ! empty($right_lat) && ! empty($right_lon)) {
+                $query->whereBetween('latitude', [min($left_lat, $right_lat), max($left_lat, $right_lat)])->whereBetween('longitude', [min($left_lon, $right_lon), max($left_lon, $right_lon)]);
+            }
 
-        if (! empty($left_lat) && ! empty($left_lon) && ! empty($right_lat) && ! empty($right_lon)) {
-            $query->whereBetween('latitude', [min($left_lat, $right_lat), max($left_lat, $right_lat)])->whereBetween('longitude', [min($left_lon, $right_lon), max($left_lon, $right_lon)]);
+            if (! empty($rooms)) {
+                $query->where('rooms', '>', $rooms);
+            }
+            if (! empty($beds)) {
+                $query->where('beds', '>', $beds);
+            }
+
+            if (! empty($services)) {
+                $query->wherehas('services', function ($q) use ($services) {
+
+                    $q->whereIn('id', $services);
+
+                });
+            }
         }
 
-        if (! empty($rooms)) {
-            $query->where('rooms', '>', $rooms);
-        }
-        if (! empty($beds)) {
-            $query->where('beds', '>', $beds);
-        }
-
-        if (! empty($services)) {
-            $query->wherehas('services', function ($q) use ($services) {
-
-                $q->whereIn('id', $services);
-
-            });
-        }
-
-        $apartments = $query->get();
+        $apartments = $query->paginate(12);
 
         return response()->json([
             'success' => true,
@@ -81,40 +85,5 @@ class ApartmentController extends Controller
                 'result' => 'apartment not found 404',
             ]);
         }
-    }
-
-    /**
-     * Return all the apartments inside a given range
-     *
-     * @param  string  $left_lat max_latitude
-     * @param  string  $left_lon max_longitude
-     * @param  string  $right_lat min_latitude
-     * @param  string  $right_lon min_longitude
-     * @return void
-     */
-    public function concerned_list($left_lat, $left_lon, $right_lat, $right_lon)
-    {
-
-        // Apartment::where('longitude' > $min_longitude, AND, 'latitude' > $min_latitude, OR, 'longitude' < $max_longitude, AND, 'latitude' < $max_latitude)->orderByDesc('sponsorship_expiration_date')->paginate('18')
-        // dd(floatval($left_lat), $left_lon, $right_lat, $right_lon);
-        $left_lat = floatval($left_lat);
-        $right_lat = floatval($right_lat);
-        $left_lon = floatval($left_lon);
-        $right_lon = floatval($right_lon);
-
-        // $apartments = Apartment::with(['services'])->whereBetween('latitude', [min($left_lat, $right_lat), max($left_lat, $right_lat)])->whereBetween('longitude', [min($left_lon, $right_lon), max($left_lon, $right_lon)])->orderByDesc('id')->get();
-        // dd($apartments);
-        $query = Apartment::query();
-
-        if (! empty($value)) {
-            $query->where('column', $value);
-        }
-
-        $query->get();
-
-        return response()->json([
-            'success' => true,
-            'result' => $apartments,
-        ]);
     }
 }
