912f294fe3f273b0b17c61574df4fcf36e91b69b
Dan97O <dan97-oddo@live.it>
2023-07-11 22:54:48 +0200

    add index views

--- resources/views/user/views/index.blade.php
+++ resources/views/user/views/index.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-6 m-auto">
-
-                <div class="card">
-                    <div class="card-header">
-                        Stats Views
-                    </div>
-                    <div class="card-body">
-                        <p>Total number of views: {{ count($views) }}</p>
-                        {{-- altre statistiche --}}
-                    </div>
-                </div>
-            </div>
-        </div>
+@section('content')
+    <div class="container p-4 bg-light rounded-5 mt-4" id="viewsContainer">
+        <a href="{{ route('user.apartments.show', $slug) }}" role="button"><i
+                class="fa-solid fa-circle-arrow-left fs-3 me-2 mt-2 btn-hover" style="color: #3FA9F5"></i></a>
+        <h2 class="text-center">Views over time</h2>
+        <canvas id="myChart"></canvas>
     </div>
 @endsection
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script type="text/javascript">
+    const views = @JSON($views);
+    const apartments = @JSON($apartments);
+    const slug = @JSON($slug);
+    console.log(views);
+</script>
