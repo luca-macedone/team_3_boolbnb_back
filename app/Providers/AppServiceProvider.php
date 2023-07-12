534e38492225f5dc51536b1e0dd4058735404cfd
luca-macedone <luca.macedone@outlook.it>
2023-07-13 01:59:16 +0200

    add braintree support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function ($app) {
+            return new Gateway([
+                'environment' => 'sandbox',
+                'merchantId' => 'gnqpz3fzybqs5dvp',
+                'publicKey' => 'yk6x27z2rpgzpwy4',
+                'privateKey' => '6ad14b637e69b3e9659c48a2e7ca1b3b',
+            ]);
+        });
     }
 }
