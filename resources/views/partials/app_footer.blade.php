5c6b766abcb4e4dc7ed48f50cf1147d3b0b5d2d8
luca-macedone <luca.macedone@outlook.it>
2023-07-14 22:22:52 +0200

    fix footer responsive rules

--- resources/views/partials/app_footer.blade.php
+++ resources/views/partials/app_footer.blade.php
@@ -1,12 +1,16 @@
 <footer id="footer">
-    <section classr">
-        <div class="container  container_widtr">
-            <div class="row my-5 pt-5 ">
+    <section class="">
+        <div class="container container_width">
+            <div class="row mt-5 pt-5">
 
-                <div class="col-6 col-lg-3">
-                    <h2>BOOL BNB</h2>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <!-- <h2>BOOL BNB</h2> -->
+                    <div class="footer_logo_wrapper d-flex justify-content-center justify-content-md-start">
+                        <img class="d-inline-flex" src="{{ asset('/storage/internal/full_logo_horizontal.svg') }}"
+                            alt="">
+                    </div>
 
-                    <ul class="d-flex flex-column gap-3 mt-4">
+                    <ul class="d-flex flex-column align-items-center align-items-md-start gap-3 mt-4">
                         <li><a href="#">Advanced search</a> </li>
                         <li><a href="#">Rent an apartment</a> </li>
                         <li><a href="#">Sponsorship</a> </li>
@@ -16,10 +20,10 @@
                     </ul>
 
                 </div>
-                <div class="col-6 col-lg-3">
-                    <h2>CONTACTS</h2>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h2 class="text-center text-md-start">CONTACTS</h2>
 
-                    <ul class="d-flex flex-column gap-3 mt-4">
+                    <ul class="d-flex flex-column align-items-center align-items-md-start gap-3 mt-4">
                         <li><a href="#"> Terms of Use</a></li>
                         <li><a href="#"> Privacy policy</a></li>
                         <li><a href="#"> BoolBnb maps</a></li>
@@ -29,10 +33,10 @@
                     </ul>
 
                 </div>
-                <div class="col-6 col-lg-3 mt-5 mt-lg-0">
-                    <h2>MADE BY</h2>
+                <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0">
+                    <h2 class="text-center text-md-start">MADE BY</h2>
 
-                    <ul class="d-flex flex-column gap-3 mt-4">
+                    <ul class="d-flex flex-column align-items-center align-items-md-start gap-3 mt-4">
                         <li class="mac"><a href="https://www.linkedin.com/in/luca-macedone/">Luca Macedone</a></li>
                         <li class="rub"><a href="https://www.linkedin.com/in/luca-ruboni/">Luca Ruboni</a></li>
                         <li class="nes"><a href="https://www.linkedin.com/in/robertonesta/">Roberto Nesta</a></li>
@@ -42,27 +46,23 @@
                     </ul>
                 </div>
 
-                <div class="col-6 col-lg-3 mt-5 mt-lg-0 d-flex justify-content-center">
+                <div class="col-12 col-md-6 col-lg-3 mt-5 mt-lg-0 d-flex justify-content-center">
                     <img class="contributors" src="{{ asset('/storage/internal/contributors.png') }}" alt="">
                 </div>
 
 
             </div>
 
-
-
-
         </div>
 
-        <div class="col-12 down_col d-flex justify-content-betweenr">
+        <div class="col-12 down_col d-flex justify-content-between">
 
             <div class="container container_width">
-                <div class="row">
+                <div class="row justify-content-center">
 
+                    <nav class="navbar my-3 d-flex justify-content-center align-items-center">
 
-                    <nav class="navbar my-3 d-flex align-items-center">
-
-                        <a class="navbar-brand d-flex text-light" href="#">
+                        <a class="navbar-brand text-end me-0 text-md-center text-light" href="#">
                             &copy; 2023 Boolbnb, Inc.
                         </a>
 
