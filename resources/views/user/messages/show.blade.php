5a2a6aecc7e9cfe1e4f277c1309cb4359cd3a21e
luca-macedone <luca.macedone@outlook.it>
2023-07-14 11:33:08 +0200

    fix modal and some style

--- resources/views/user/messages/show.blade.php
+++ resources/views/user/messages/show.blade.php
@@ -4,17 +4,17 @@
     <div class="container">
         @include('partials.session_message')
 
-        <div class="p-5 mb-4 rounded-3 shadow mt-3 dashboard_jumbotron">
-            <div class="container-fluid d-flex flex-column justify-content-between gap-5">
-                <div class="pb-5">
-                    <h1 class="display-5 fw-bold">
+        <div class="px-5 py-3 mb-4 rounded-3 shadow mt-3 dashboard_jumbotron">
+            <div class="container-fluid d-flex flex-column justify-content-between gap-2">
+                <div class="pb-3">
+                    <h1 class="fs-1 fw-semibold">
                         Message
                     </h1>
                     <p class="col-md-8 fs-5">
                         {{ __('Here you can view the individual message received and delete it.') }}
                     </p>
                 </div>
-                <div class="pt-5 d-flex justify-content-between align-items-center">
+                <div class="d-flex justify-content-between align-items-center">
                     <a type="button" class="btn back_btn d-flex align-items-center gap-2 shadow"
                         href="{{ route('user.messages.index', $message->apartment->slug) }}">
                         <i class="fa-solid fa-arrow-left-long"></i>
@@ -24,46 +24,71 @@
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-header d-flex justify-content-between">
+        <div class="card message_card border-0 shadow">
+            <div class="card_header p-3 pb-2 border-bottom d-flex justify-content-between">
                 <span>Message Details</span>
-                <span>From: {{ $message->email }}</span>
+                @if ($message->name && $message->lastname)
+                    <span> from </span>
+                    <strong class="fw-semibold"> {{ $message->name . ' ' . $message->lastname }}</strong>
+                @else
+                @endif
+                <span>Email: <span class="text_italic">{{ $message->email }}</span></span>
             </div>
-            <div class="card-body">
-                <div class="mb-3">
+            <div class="card-body d-flex justify-content-between align-items-start">
+                <div class="me-3">
                     <div>{{ $message->message }}</div>
                 </div>
-                <div class="d-flex justify-content-end">
-                    <button class="action_btn action_delete p-2" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </div>
+                {{-- delete btn --}}
+                <button class="action_btn action_delete p-2" title="Delete apartment" title="{{ __('Delete') }}"
+                    id="open_modal_btn">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
             </div>
         </div>
 
 
-        <!-- Delete Modal -->
-        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title text-danger" id="{{ 'modalTitle' . $message->id }}"> {{ __('Danger Zone') }}
-                        </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Are you sure you want to delete this message?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <form action="{{ route('user.messages.destroy', $message) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </div>
+
+
+        {{-- delete modal --}}
+        <div class="d-none delete_modal shadow" id="delete_apartment_modal">
+            <div class="h-100 w-100 d-flex flex-column justify-content-center align-items-center gap-5 p-5">
+                <div class="d-flex flex-column gap-2">
+                    <strong class="text-danger">This operation is irreversible!</strong>
+                    <span>Are you sure you want to proceed?</span>
+                </div>
+
+                <div class="d-flex align-items-center gap-2">
+                    <button class="btn btn-outline-dark" id="close_modal_btn">
+                        close
+                    </button>
+
+                    <form action="{{ route('user.messages.destroy', $message) }}" method="post" class="">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="rounded btn btn-outline-danger">
+
+                            {{ __('Delete permanently') }}
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        const open_modal_bnt = document.getElementById('open_modal_btn');
+        const close_modal_bnt = document.getElementById('close_modal_btn');
+        const modal_el = document.querySelector('#delete_apartment_modal');
+
+        open_modal_bnt.addEventListener('click', e => {
+            // console.log('open')
+            modal_el.classList.toggle('d-none');
+
+        })
+
+        close_modal_bnt.addEventListener('click', e => {
+            // console.log('close')
+            modal_el.classList.toggle('d-none');
+        })
+    </script>
 @endsection
