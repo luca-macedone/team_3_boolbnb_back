c5ad68e3aa65182e574549983dcfdcea6542906e
Dan97O <dan97-oddo@live.it>
2023-07-15 18:51:21 +0200

    add button close validation errors

--- resources/views/partials/validation_errors.blade.php
+++ resources/views/partials/validation_errors.blade.php
@@ -1,7 +1,9 @@
 @if ($errors->any())
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         <ul>
             @foreach ($errors->all() as $error)
+                <strong>Error! </strong>
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
